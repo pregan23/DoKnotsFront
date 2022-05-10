841c60f3c54d9664c55a3b3c0918d4e02686be87
pregan23 <pregan2bing@gmail.com>
2022-05-10 11:28:45 -0400

    registration component bare

--- doknot-front/src/App.vue
+++ doknot-front/src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RegisterUser />
+  </div>
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import RegisterUser from './components/RegisterUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,  
+    RegisterUser
+}
 }
 </script>
 
---
+++ doknot-front/src/components/RegisterUser.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <form class="regform">
+        <input placeholder='Username'/>
+        <input placeholder="Password" />
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+   name:'RegisterUser',
+   props: {} 
+}
+</script>
+
+<style scoped>
+.regform {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    }
+input {
+    width: 30%;
+    text-align: center;
+}
+</style>
