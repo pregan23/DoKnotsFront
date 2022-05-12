3ceb626bfe56ba382356f773549632bea7b582ac
pregan23 <pregan2bing@gmail.com>
2022-05-12 17:46:01 -0400

    rendering just fine, styling will be hell lol

--- doknot-front/src/components/UserLanding.vue
+++ doknot-front/src/components/UserLanding.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
-        <div v-if="doKnotsWithStreaks">
+        <div v-if="doKnotsWithStreaks" id="doknot">
             <h2>Here's what you've been working on lately</h2>
             <div :key="doKnot.id" v-for="doKnot in doKnotsWithStreaks">
                 <h3>Have you been {{ doKnot.habit }}?</h3>
                 <h4>{{ doKnot.alternatives }}</h4>
+                <div :key="streak.id" v-for="streak in doKnot.Streaks">
+                    <h3>{{ streak.howLong }}</h3>
+                    <h4 v-if="streak.isActive">Ongoing</h4>
+                </div>
             </div>
         </div>
         <div v-if="userEntries">
